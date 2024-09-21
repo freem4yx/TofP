8058714565edc7282743da7901601141e38f7c7d
freem4yx <freem4yx@yandex.ru>
2024-09-21 20:09:55 +0300

    division

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main()
     printf("(%d + %d) mod 5 = %d\n", a, b, (a + b) % 5);
     printf("(%d - %d) mod 2 = %d\n", a, b, (a - b) % 2);
     printf("%d * %d = %d\n", a, b, a * b);
-
+    printf("%d / %d = %d\n", a, b, a / b);
+    
     return 0;
 }
