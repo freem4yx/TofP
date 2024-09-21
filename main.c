bc052ad76d3f84bf0fc516799272aac58ac2de95
freem4yx <freem4yx@yandex.ru>
2024-09-21 16:45:32 +0300

    add mod(3)

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main()
     printf("Enter two numbers, please:\n");
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%d + %d = %d", a, b, a + b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("(%d + %d) mod 3 = %d\n", a, b, (a + b) % 3);
     return 0;
 }
