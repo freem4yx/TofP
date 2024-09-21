c4f148a19ccf7becc46e11ae098cf9950ebbe5aa
freem4yx <freem4yx@yandex.ru>
2024-09-21 21:10:12 +0300

    float_fix

--- file.c
+++ file.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-<<<<<<< HEAD
+
     float x;
     float y;
     printf("Enter the number and power:\n");
     scanf("%f %f", &x, &y);
-    printf("%.1f ^ %.1f = %.1f\n", x, y, pow(x,y));
+    printf("%f ^ %f = %.4f\n", x, y, pow(x,y));
     printf("Enter the number and base:\n");
     scanf("%.1f %.1f", &x, &y);
     printf("log[%.1f] (%.1f) = %.1f\n", x, y, log(x) / log(y));
@@ -16,12 +16,5 @@ int main()
     printf("pi = %f\n", M_PI);
     printf("e = %f\n", exp(1));
     
-=======
-    int x;
-    int y;
-    printf("Enter the number and power:\n");
-    scanf("%d %d", &x, &y);
-    printf("%d ^ %d = %d\n", x, y, (int) pow(x,y));
->>>>>>> 3d92ac8 (Entering of numbers)
     return 0;
 }
