47a4c63785ca9c5cd241e527e1148251992555a7
freem4yx <freem4yx@yandex.ru>
2024-09-21 21:05:22 +0300

    logarithm

--- file.c
+++ file.c
@@ -7,7 +7,12 @@ int main()
     int y = 0;
     printf("%d ^ %d = %d\n", x, y, (int) pow(x,y));
 
+    printf("Enter the number and base:\n");
+    scanf("%d %d", &x, &y);
+    printf("log[%d] (%d) = %.1f\n", x, y, log(x) / log(y));
 
     printf("pi = %f\n", M_PI);
     printf("e = %f\n", exp(1));
+    
+    return 0;
 }
